121959fbec71a8e1772c47faf60c4c5e6bf1f6d8
Felix Kybranz <felixkybranz@me.com>
2017-07-03 16:40:43 +0200

    added lines to create stack

--- create-stack.sh
+++ create-stack.sh
@@ -5,4 +5,8 @@ test $# = 1 || { echo "Need 1 parameter: Name of the stack to create"; exit 1; }
 STACK="$1"
 
 # Create the stack using server-landscape.yaml and defining all necessary parameters
-[[TODO]]
+#[[TODO]]
+# server-landscape.yaml params: <external net/key_pair/flavor/image/zone>
+# server.yaml params <name/key_pair/flavour/image/network/zone/security_groups/init_code>
+
+./cc-openstack stack create $STACK -t server-landscape.yaml --parameter "external_net=externalNET;key_pair=kybranz;flavor=Cloud Computing;image=ubuntu-16.04;zone=Cloud Computing 2017"
